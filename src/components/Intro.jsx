616fc7e7aca8c67ba36ec7732ad4203a3f34a69a
Taylan Turker <taylanturker@Taylans-MacBook-Pro.local>
2024-06-07 07:13:58 +0300

    Add sticky-footer changes

---
+++ src/components/Intro.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Intro() {
+  return (
+    <div className='h-screen flex text-[2vw] items-center justify-center'>
+        <h2 className='max-w-[45%] text-center leading-none'>This is an example of a sticky footer. Scroll down to inspect</h2>
+    </div>
+  )
+}
